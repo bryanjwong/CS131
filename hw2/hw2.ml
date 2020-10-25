501f69f57f9f2489503c4763fa210fca3e9a4ad2
Bryan Wong <bryanjwong@g.ucla.edu>
2020-10-25 03:25:15 -0700

    initial implementation of problem 3

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -26,6 +26,34 @@ let parse_tree_leaves tree =
         | Leaf x -> [x]
         | Node (_, b) -> helper b
 
+(* Problem 3 *)
+let make_matcher (top, rules) =
 
+    let rec check_prefix a b =
+        if a = [] then Some a
+        else if b = [] then None
+        else if (List.hd a) != (List.hd b) then None
+        else check_prefix (List.tl a) (List.tl b)
+    in
+
+    (* Sub out the nonterminal for the rule *)
+    let rec generate_expr sym_rules tail build frag =
+        match sym_rules with 
+            | [] -> None
+            | hd::tl -> match (evaluate_expr (hd@tail) build frag) with
+                            | Some x -> Some x
+                            | None -> generate_expr tl tail build frag
+
+    (* Build the possible instruction and check if it's a prefix *)
+    and evaluate_expr expr build frag =
+        match expr with
+            | [] -> check_prefix build frag
+            | (T hd)::tl -> evaluate_expr tl (build@[hd]) frag
+            | (N hd)::tl ->
+                let hd_rules = rules hd in
+                generate_expr hd_rules tl build frag
+    in
+
+    evaluate_expr [N top] []
 
 
